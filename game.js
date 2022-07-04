eb719674a0ed25c25bda5cfb0302b0885af0dac7
Dilip <19bec012@iiitdwd.ac.in>
2022-07-04 15:26:54 +0530

    First Commit

---
+++ game.js
@@ -0,0 +1,9 @@
+alert("Welcome nhi karoge hamara!!");
+
+function nextSequence(){
+    var randomNumber = Math.floor(Math.random()*4);
+    return randomNumber;
+    
+}
+var r = nextSequence();
+console.log(r);
